fd56cac50d2f8661b0a404f7acf41ea4079dcab2
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-17 16:42:26 +0500

    fix: head content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ interface RootLayoutProps {
 
 const RootLayout = ({ children }: RootLayoutProps) => (
   <html lang="en">
+    <head>
+      <title>BRB Titans - The best place for your banking needs.</title>
+      <meta name="description" content="BRB Titans - The best place for your banking needs." />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="UTF-8" />
+    </head>
     <body>
       {children}
     </body>
