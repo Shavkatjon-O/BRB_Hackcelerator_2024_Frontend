82380a81c9118339fbd1ccd71be4e0b2f31dfa5b
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-20 18:24:47 +0500

    fix: pages

--- src/lib/coreApi.ts
+++ src/lib/coreApi.ts
@@ -1,31 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Create an instance of Axios with default settings
 const CoreAPI = axios.create({
   baseURL: process.env.BACKEND_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
-
-// Function to get the CSRF token from cookies
-const getCsrfToken = () => {
-  const match = document.cookie.match(new RegExp('(^| )csrftoken=([^;]+)'));
-  return match ? match[2] : null;
-};
-
-// Add a request interceptor to include the CSRF token
-CoreAPI.interceptors.request.use(
-  (config) => {
-    const csrfToken = getCsrfToken();
-    if (csrfToken) {
-      config.headers['X-CSRFToken'] = csrfToken;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default CoreAPI;
