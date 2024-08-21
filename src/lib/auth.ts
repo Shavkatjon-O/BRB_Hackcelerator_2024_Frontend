eb930248647f7ed6aa8e8377448689648edd5412
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-21 15:50:07 +0500

    fix: pages

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,34 +3,6 @@
 import CoreAPI from '@/lib/coreApi';
 import Cookies from 'js-cookie';
 
-export const signup = async (email: string, password: string) => {
-  try {
-    const response = await CoreAPI.post('/users/signup/', { email, password });
-    
-    Cookies.set('access_token', response.data.access);
-    Cookies.set('refresh_token', response.data.refresh);
-
-    return { success: 'Account created successfully!' };
-  } catch (error) {
-    console.error('Error creating account:', error);
-    return { error: 'Error creating account!' };
-  }
-};
-
-export const login = async (email: string, password: string) => {
-  try {
-    const response = await CoreAPI.post('/users/token/', { email, password });
-    
-    Cookies.set('access_token', response.data.access);
-    Cookies.set('refresh_token', response.data.refresh);
-
-    return { success: true };
-  } catch (error) {
-    console.error('Invalid email or password:', error);
-    return { error: 'Invalid email or password' };
-  }
-};
-
 export const refreshToken = async () => {
   try {
     const refresh = Cookies.get('refresh_token');
