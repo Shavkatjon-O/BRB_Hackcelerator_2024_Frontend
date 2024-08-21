cc8b34ad329e7e904724a5452f777829bee3258b
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-21 13:15:26 +0500

    feat: auth

--- src/lib/coreApi.ts
+++ src/lib/coreApi.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const CoreAPI = axios.create({
   baseURL: process.env.BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+CoreAPI.interceptors.request.use((config) => {
+  const token = Cookies.get('access_token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default CoreAPI;
