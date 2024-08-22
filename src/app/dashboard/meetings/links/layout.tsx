bb43d6c3c04b445f599fa2f1c05efee628494b7a
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-22 21:22:08 +0500

    fix

--- src/app/dashboard/meetings/links/layout.tsx
+++ src/app/dashboard/meetings/links/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 import Header from "@/app/dashboard/_components/header";
-import Sidebar from "@/app/dashboard/_components/sideBar";
+import Sidebar from "@/app/dashboard/_components/sidebar";
 
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
