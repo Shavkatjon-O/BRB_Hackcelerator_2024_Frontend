734feef66c504d6bd271e96c2ca175090c7c9c04
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-22 20:16:14 +0500

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,16 @@
 
 import withAuth from "@/components/auth/withAuth";
 
+// import { currentUser } from "@/hooks/useUser";
+import { useUser } from "@/hooks/useUser";
 
 const DashboardPage = () => {
+  const { user } = useUser();
+
   return (
     <>
+      <h1>Welcome {user?.email}</h1>
+
       <h1>Dashboard</h1>
     </>
   );
