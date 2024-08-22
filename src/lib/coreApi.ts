bf342da899532f245c68c56c29afa81778cc3057
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-22 21:14:43 +0500

    fix

--- src/lib/coreApi.ts
+++ src/lib/coreApi.ts
@@ -1,18 +1,49 @@
-import axios from "axios";
-import Cookies from "js-cookie";
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// const CoreAPI = axios.create({
+//   baseURL: process.env.BACKEND_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// CoreAPI.interceptors.request.use((config) => {
+//   const token = Cookies.get('access_token');
+//   if (token) {
+//     config.headers['Authorization'] = `Bearer ${token}`;
+//   }
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
+
+// export default CoreAPI;
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const CoreAPI = axios.create({
   baseURL: process.env.BACKEND_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 CoreAPI.interceptors.request.use((config) => {
-  const token = Cookies.get('access_token');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+  const accessToken = Cookies.get('access_token');
+  const csrfToken = Cookies.get('csrftoken'); // Make sure the CSRF token cookie name matches your setup
+  console.log('CSRF Token:', Cookies.get('csrftoken'));
+
+
+  if (accessToken) {
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
   }
+  
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+  
   return config;
 }, (error) => {
   return Promise.reject(error);
--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -3,16 +3,16 @@
 import coreApi from '@/lib/coreApi';
 
 export const signInService = async (email: string, password: string) => {
-  const response = await coreApi.post('/token/', { email, password });
+  const response = await coreApi.post('/users/token/', { email, password });
   return response.data;
 };
 
 export const signUpService = async (email: string, password: string) => {
-  const response = await coreApi.post('/signup/', { email, password });
+  const response = await coreApi.post('/users/signup/', { email, password });
   return response.data;
 };
 
 export const refreshTokenService = async (refreshToken: string) => {
-  const response = await coreApi.post('/token/refresh/', { refresh: refreshToken });
+  const response = await coreApi.post('/users/token/refresh/', { refresh: refreshToken });
   return response.data;
 };
