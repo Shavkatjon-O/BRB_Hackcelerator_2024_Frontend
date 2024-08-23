634a32d2f2a299901219ec3c34cfa160504bbe7c
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 11:26:05 +0500

    fix

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,16 @@
 import Header from './_components/header';
 import Footer from './_components/footer';
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <ClerkProvider>
       <Header />
       <main>{children}</main>
       <Footer />
+      </ClerkProvider>
     </>
   );
 };
