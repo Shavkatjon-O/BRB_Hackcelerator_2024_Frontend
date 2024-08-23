b75d0aa9e928435ae08bc0edba4e9bad63410e6c
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 17:55:05 +0500

    fix

---
+++ src/app/dashboard/calendar/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const DashboardPage = () => {
+  return (
+    <>
+      <h1>Calendar</h1>
+    </>
+  );
+}
+
+export default DashboardPage;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const DashboardPage = () => {
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+}
+
+export default DashboardPage;
