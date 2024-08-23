01c78fc017465fac5bacc0288bb21da15323999a
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 16:59:20 +0500

    fix: content area

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <Header />
         <div className="flex w-full pt-16 h-screen">
           <Sidebar />
-          <main className='p-4 overflow-y-scroll'>{children}</main>
+          <main className='p-4 w-full overflow-y-scroll'>{children}</main>
         </div>
       </div>
     </>
