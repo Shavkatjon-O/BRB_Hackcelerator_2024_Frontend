d747a7b6824c951cb158b67163035fe8da577d7f
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 17:55:58 +0500

    fix

--- src/app/dashboard/_components/sidebar.tsx
+++ src/app/dashboard/_components/sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="#">
+            <Link href="/dashboard/calendar">
               <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
                 <Calendar className="w-5 h-5 mr-3" />
                 Calendar
@@ -25,7 +25,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="#">
+            <Link href="/dashboard/meetings">
               <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
                 <ClipboardList className="w-5 h-5 mr-3" />
                 Meetings
@@ -33,7 +33,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="#">
+            <Link href="/dashboard/settings">
               <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
                 <Settings className="w-5 h-5 mr-3" />
                 Settings
