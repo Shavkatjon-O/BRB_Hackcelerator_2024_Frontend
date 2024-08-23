d882609bc9ebaf457613478ca04e825a6ee57f72
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 10:57:47 +0500

    fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,6 @@
 import Sidebar from './_components/sidebar';
 import Header from './_components/header';
 
-import StreamVideoProvider from "@/providers/StreamVideoClient";
-
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
@@ -11,9 +8,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <Header />
         <div className="flex w-full pt-16 h-screen">
           <Sidebar />
-          <StreamVideoProvider>
           <main className='p-4'>{children}</main>
-          </StreamVideoProvider>
         </div>
       </div>
     </>
