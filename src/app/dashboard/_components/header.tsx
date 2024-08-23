c9d3f0a3ac67aca23b747fe25f6dbf7691351a4b
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 15:12:48 +0500

    fix: dashboard

--- src/app/dashboard/_components/header.tsx
+++ src/app/dashboard/_components/header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { Button } from "@/components/ui/button"
 
 import Link from "next/link"
 import Image from "next/image"
 
+import useUser from "@/hooks/useUser";
+import { User } from "lucide-react";
+
 const Header = () => {
+  const { user } = useUser();
+
   return (
-    <header className="h-16 px-4 absolute top-0 left-0 right-0 border-b flex justify-between items-center border-b-gray-300">
+    <header className="h-16 px-8 absolute top-0 left-0 right-0 border-b flex justify-between items-center border-b-gray-300">
       <Link href="/" className="text-2xl font-bold flex items-center">
         <Image 
           src="/brb-titans-logo.png"
@@ -17,9 +24,14 @@ const Header = () => {
         BRB Titans
       </Link>
 
-      <Link href="/sign-out">
+      <div className="flex items-center">
+        <User className="w-6 h-6 mr-2" />
+        <span className="font-semibold">{ user ? user.email : "Loading" }</span>
+      </div>
+
+      {/* <Link href="/sign-out">
         <Button className="font-semibold">Sign Out</Button>
-      </Link>
+      </Link> */}
     </header>
   );
 };
--- src/app/dashboard/_components/sidebar.tsx
+++ src/app/dashboard/_components/sidebar.tsx
@@ -1,53 +1,60 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
-import { Calendar, ClipboardList, Settings, User, Grid } from 'lucide-react';
+import { Home, Calendar, ClipboardList, Settings, User } from 'lucide-react';
 
 const Sidebar = () => {
   return (
-    <aside className={cn('w-60 p-4 border-r border-gray-300')}>
-      <ul className="space-y-2">
-        <li>
-          <Link href="#">
-            <Button variant="ghost" className="flex items-center justify-start w-full text-left">
-              <Grid className="w-5 h-5 mr-3" />
-              Dashboard
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <Button variant="ghost" className="flex items-center justify-start w-full text-left">
-              <Calendar className="w-5 h-5 mr-3" />
-              Calendar
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <Button variant="ghost" className="flex items-center justify-start w-full text-left">
-              <ClipboardList className="w-5 h-5 mr-3" />
-              Meetings
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <Button variant="ghost" className="flex items-center justify-start w-full text-left">
-              <Settings className="w-5 h-5 mr-3" />
-              Settings
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <Button variant="ghost" className="flex items-center justify-start w-full text-left">
-              <User className="w-5 h-5 mr-3" />
-              Profile
-            </Button>
-          </Link>
-        </li>
-      </ul>
+    <aside className={cn('h-full w-60 bg-gray-900 text-white p-4')}>
+      <div className="flex flex-col justify-between h-full">
+        <ul className="space-y-4">
+          <li>
+            <Link href="#">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <Home className="w-5 h-5 mr-3" />
+                Dashboard
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <Calendar className="w-5 h-5 mr-3" />
+                Calendar
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <ClipboardList className="w-5 h-5 mr-3" />
+                Meetings
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <Settings className="w-5 h-5 mr-3" />
+                Settings
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <User className="w-5 h-5 mr-3" />
+                Profile
+              </Button>
+            </Link>
+          </li>
+        </ul>
+        <div className="mt-8">
+          <Button variant="destructive" className="w-full text-left text-white font-semibold">
+            Sign out
+          </Button>
+        </div>
+      </div>
     </aside>
   );
 };
