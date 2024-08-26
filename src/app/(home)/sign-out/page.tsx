355894bc386cc1e43c261654dcaf3d2f35a84db5
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-26 11:08:33 +0500

    fix: sign out page

--- src/app/(home)/sign-out/page.tsx
+++ src/app/(home)/sign-out/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { useEffect } from 'react';
-import Cookies from 'js-cookie';
+import { Loader } from "lucide-react";
+import React, { useEffect } from "react";
+import Cookies from "js-cookie";
 
-const LogoutPage = () => {
+const SignOutPage = () => {
   useEffect(() => {
-    Cookies.remove('access_token');
-    Cookies.remove('refresh_token');
-    
-    if (typeof window !== 'undefined') {
-      window.location.href = '/';
-    }
+    Cookies.remove("access_token");
+    Cookies.remove("refresh_token");
+
+    window.location.href = "/";
   }, []);
 
-  return null;
+  return (
+    <div className="absolute top-0 bottom-0 right-0 left-0 z-50">
+      <Loader className="w-6 h-6" /> Signing out...
+    </div>
+  );
 };
 
-export default LogoutPage;
+export default SignOutPage;
