cfcc685dbb80cb9d1032482013395dcf5a544919
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-26 11:00:34 +0500

    fix: sign up page

--- src/app/(home)/sign-up/page.tsx
+++ src/app/(home)/sign-up/page.tsx
@@ -1,101 +1,144 @@
 "use client";
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+import CoreAPI from '@/lib/coreApi';
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 
+import { signUpSchema } from './schemas';
+
+import { Loader, X } from 'lucide-react';
+
 const SignUpPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (password !== confirmPassword) {
-      setError('Passwords do not match');
+    const validatedResult = signUpSchema.safeParse({ email, password, confirmPassword });
+
+    if (!validatedResult.success) {
+
+      validatedResult.error.issues.forEach((issue, index) => {
+        setTimeout(() => {
+          toast("Error", {
+            description: issue.message || "An error occurred during sign-up.",
+            action: {
+              label: <X className='w-4 h-4' />,
+              onClick: () => console.log("Close"),
+            },
+          });
+        }, index * 100);
+      });
       return;
     }
+    
+    setLoading(true);
 
-    const result = await signUp(email, password);
-    if (result.success) {
+    try {
+      const response = await CoreAPI.post("/users/sign-up/", { email, password });
       
-      setSuccess(result.message);
-
-      window.location.href = '/sign-in';
-    } else {
-      setError(result.message);
+      if (response.status === 201) {
+        toast("Success", {
+          description: "Sign-up successful! Redirecting...",
+          action: {
+            label: <X className='w-4 h-4' />,
+            onClick: () => console.log("Close"),
+          },
+        });
+        router.push('/sign-in');
+      } else {
+        toast("Error", {
+          description: response.data.detail || "An error occurred during sign-up.",
+          action: {
+            label: <X className='w-4 h-4' />,
+            onClick: () => console.log("Close"),
+          },
+        });
+      }
+    } catch (error: any) {
+      const errorMessage = error.response?.data?.detail || "An error occurred while signing up. Please try again.";
+      toast("Error", {
+        description: errorMessage,
+        action: {
+          label: <X className='w-4 h-4' />,
+          onClick: () => console.log("Close"),
+        },
+      });
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <>
-      <div className='h-screen flex justify-center items-center'>
-        <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-2xl font-bold mb-6">Sign Up</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <Label htmlFor="email" className="block mb-3">Email</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
-                className="w-full"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <Label htmlFor="password" className="block mb-3">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Create a password"
-                className="w-full"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <Label htmlFor="confirmPassword" className="block mb-3">Confirm Password</Label>
-              <Input
-                type="password"
-                id="confirmPassword"
-                placeholder="Confirm your password"
-                className="w-full"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-            <Button type="submit" className="w-full bg-gray-800 text-white">Sign Up</Button>
-            {error && (
-              <div className='mt-4 p-1.5 flex justify-center items-center border border-red-500 text-red-600 rounded-lg'>
-                <p>{error}</p>
-              </div>
-            )}
-            {success && (
-              <div className='mt-4 p-1.5 flex justify-center items-center border border-green-500 text-green-600 rounded-lg'>
-                <p>{success}</p>
-              </div>
-            )}
-            <div className="text-center mt-4">
-              <p className="text-sm text-gray-600">
-                Already have an account?{' '}
-                <a href="/sign-in" className="text-blue-600 hover:underline">
-                  Sign in!
-                </a>
-              </p>
-            </div>
-          </form>
-        </div>
+    <div className='h-screen flex justify-center items-center'>
+      <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-sm border">
+        <h1 className="text-2xl font-bold mb-6">Sign up</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <Label htmlFor="email" className="block mb-3">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              className="w-full"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <Label htmlFor="password" className="block mb-3">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              className="w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <Label htmlFor="confirmPassword" className="block mb-3">Confirm Password</Label>
+            <Input
+              type="password"
+              id="confirmPassword"
+              placeholder="Confirm your password"
+              className="w-full"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <Button type="submit" className="w-full bg-gray-800 text-white" disabled={loading}>
+            {loading ? (
+              <>
+                <Loader className='w-5 h-5 mr-1' /> Signing up...
+              </>
+             ) : (
+              'Sign up'
+             )}
+          </Button>
+          <div className="text-center mt-4">
+            <p className="text-sm text-gray-600">
+              Already have an account?{' '}
+              <a href="/sign-in" className="text-blue-600 hover:underline">
+                Sign in!
+              </a>
+            </p>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
 export default SignUpPage;
-
