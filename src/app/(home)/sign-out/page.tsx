13b7483d472353dddd6e5a92b6a5d03d68b643af
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-26 13:29:20 +0500

    fix: loader

--- src/app/(home)/sign-out/page.tsx
+++ src/app/(home)/sign-out/page.tsx
@@ -13,7 +13,7 @@ const SignOutPage = () => {
   }, []);
 
   return (
-    <div className="absolute top-0 bottom-0 right-0 left-0 z-50">
+    <div className="absolute top-0 bottom-0 right-0 left-0 z-50 bg-white flex justify-center items-center">
       <Loader className="w-6 h-6" /> Signing out...
     </div>
   );
