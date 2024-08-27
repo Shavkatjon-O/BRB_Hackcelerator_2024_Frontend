c939319f6380d996ae6bc63c36f6fb9d64654ddb
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-27 13:21:59 +0500

    fix: auth page background

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./_components/Footer";
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="min-h-screen flex flex-col justify-between">
+      <div className="min-h-screen flex flex-col justify-between bg-slate-100">
         <Header />
           <main>{children}</main>
         <Footer />
