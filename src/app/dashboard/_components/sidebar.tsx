37d2d8056f5b97564a4715447a53ea352f5ab430
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-27 17:22:04 +0500

    fix: dashboard design

--- src/app/dashboard/_components/sidebar.tsx
+++ src/app/dashboard/_components/sidebar.tsx
@@ -33,6 +33,15 @@ const Sidebar = () => {
               </Button>
             </Link>
           </li>
+
+          <li>
+            <Link href="/dashboard/messenger">
+              <Button variant="ghost" className="flex items-center justify-start w-full text-left text-white">
+                <ClipboardList className="w-5 h-5 mr-3" />
+                Messenger
+              </Button>
+            </Link>
+          </li>
           
           <li>
             <Link href="/dashboard/settings">
---
+++ src/app/dashboard/messenger/page.tsx
@@ -0,0 +1,9 @@
+const MessengerPage = () => {
+  return (
+    <div>
+      <h1>Messenger</h1>
+    </div>
+  );
+}
+
+export default MessengerPage;
