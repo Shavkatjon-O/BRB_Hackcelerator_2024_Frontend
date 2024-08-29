6de483c481095783e085d698a58cd1cb4af87a53
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-29 21:07:50 +0500

    fix: events page

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { EventType } from "./_types/event";
 import EventFormDialog from "./_components/add-event-dialog";
 import Calendar from "./_components/calendar";
+
+import { getEventsList, createEvent } from "@/services/eventsServices";
 
 const CalendarPage = () => {
   const [events, setEvents] = useState<EventType[]>([]); // State to manage events
@@ -11,9 +13,28 @@ const CalendarPage = () => {
   const [currentDate, setCurrentDate] = useState(new Date()); // State to manage the current date
   const [isDialogOpen, setIsDialogOpen] = useState(false); // State to manage dialog visibility
 
-  const handleAddEvent = (e: React.FormEvent<HTMLFormElement>, eventData: Partial<EventType>) => {
-    setEvents([...events, { ...eventData, id: events.length + 1 } as EventType]);
-    setIsDialogOpen(false);
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await getEventsList();
+        setEvents(response);
+      } catch (error) {
+        console.error("Failed to fetch events", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const handleAddEvent = async (e: React.FormEvent<HTMLFormElement>, eventData: Partial<EventType>) => {
+    e.preventDefault();
+    try {
+      const newEvent = await createEvent(eventData);
+      setEvents([...events, newEvent]);
+      setIsDialogOpen(false);
+    } catch (error) {
+      console.error("Failed to create event", error);
+    }
   };
 
   const handlePrevClick = () => {
