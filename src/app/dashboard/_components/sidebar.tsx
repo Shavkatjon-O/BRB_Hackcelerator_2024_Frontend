2e1c2e61bc3695a34513411531402e6ef106f311
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-04 00:37:12 +0500

    fix: sidebar

--- src/app/dashboard/_components/sidebar.tsx
+++ src/app/dashboard/_components/sidebar.tsx
@@ -40,13 +40,7 @@ const Sidebar = () => {
   const { user, isLoaded, error } = useUser();
 
   return (
-    <aside
-      className={
-        `h-screen border-r transition-all duration-300 ease-in-out shadow-sm bg-white dark:bg-slate-950 ${
-          isOpen ? "w-64" : "w-20"
-        }`
-      }
-    >
+    <aside className={`h-screen border-r transition-all duration-300 ease-in-out shadow-sm bg-white dark:bg-slate-950 ${ isOpen ? "w-64" : "w-20"}`}>
       <div className="h-full flex flex-col justify-between">
         <div className="p-4 flex justify-between items-center">
           <Link href="/" className={`flex items-center text-lg font-bold transition-opacity duration-300 ${ isOpen ? "block" : "hidden" }`}>
@@ -60,7 +54,9 @@ const Sidebar = () => {
             />
             <span className="ml-2 text-nowrap">BRB Titans</span>
           </Link>
-          <Button variant="ghost" className="w-12 h-12 p-2"
+          <Button 
+            variant="ghost" 
+            className="w-12 h-12 p-2"
             onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? <ChevronFirst /> : <ChevronLast />}
