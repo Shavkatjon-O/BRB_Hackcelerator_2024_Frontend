f684b553db9923bcf5bf013d97e1854f11bd70fd
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-05 21:11:49 +0500

    feat: users list

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -1,97 +1,44 @@
-// import { cookies } from "next/headers";
-// import { ChatLayout } from "./_components/chat/chat-layout";
+"use client";
 
-// export default function Home() {
-//   const layout = cookies().get("react-resizable-panels:layout");
-//   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
-//   return (
-//     <div className="h-full">
-//       <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
-//     </div>
-//   );
-// }
+import { DefaultUserAvatar } from "./_components/DefaultUserAvatar";
+import coreApi from "@/lib/coreApi";
 
-// "use client";
-
-// import { 
-//   getUsers, 
-//   getMessages, 
-//   UserType 
-// } from "./_services/chatServices";
-// import { useEffect, useState } from "react";
-// import { DefaultUserAvatar } from "./_components/DefaultUserAvatar";
-
-
-// const Page = () => {
-//   const [users, setUsers] = useState<UserType[]>([]);
-//   const [messages, setMessages] = useState([]);
-
-//   const handleUserClick = (userId: number) => {
-//     getMessages(userId).then((response) => {
-//       setMessages(response.data);
-//       console.log(response.data);
-//     });
-//   }
-
-//   useEffect(() => {
-//     getUsers().then((response) => {
-//       setUsers(response.data);
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Users</h1>
-//       <ul>
-//         {/* {users.map((user) => (
-//           <li key={user.id}>
-//             <DefaultUserAvatar />
-//             {user.first_name} {user.last_name}
-//           </li>
-//         ))} */}
-
-//         {users.map((user) => (
-//           <li key={user.id} onClick={() => handleUserClick(user.id)}>
-//             <DefaultUserAvatar />
-//             {user.first_name} {user.last_name}
-//           </li>
-//         ))} 
-
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Page;
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+interface UserType {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  image: string;
+}
 
 const Page = () => {
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    coreApi.get("/chats/users/").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
-    <Tabs defaultValue="account">
-      
-      <div className="flex-col">
-
-      <TabsList className="mt-10 ml-10">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-
-      <div className="w-full bg-red-600">
-      <TabsContent value="account">
-        <div className="w-full h-full p-4 bg-green-600">
-            asdfas
-        </div>
-
-      </TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
-      </div>
-      </div>
-      
-
-    </Tabs>
-
-  )
+    <div className="flex flex-col space-y-4 p-4">
+      {users.map((user) => (
+        <Link key={user.id} href={`/dashboard/messages/${user.id}`}>
+          <div className="w-full flex items-center space-x-4 border p-4">
+            <DefaultUserAvatar />
+            <div className="flex flex-col">
+              <span>{user.first_name} {user.last_name}</span>
+              <span className="text-sm">{user.email}</span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Page;
