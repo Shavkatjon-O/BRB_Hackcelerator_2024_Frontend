956478fb063f7d9d037083739526f4e8efd90df8
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-05 20:12:01 +0500

    fix: pages

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -12,55 +12,86 @@
 //   );
 // }
 
-"use client";
+// "use client";
 
-import { 
-  getUsers, 
-  getMessages, 
-  UserType 
-} from "./_services/chatServices";
-import { useEffect, useState } from "react";
-import { DefaultUserAvatar } from "./_components/DefaultUserAvatar";
+// import { 
+//   getUsers, 
+//   getMessages, 
+//   UserType 
+// } from "./_services/chatServices";
+// import { useEffect, useState } from "react";
+// import { DefaultUserAvatar } from "./_components/DefaultUserAvatar";
 
+
+// const Page = () => {
+//   const [users, setUsers] = useState<UserType[]>([]);
+//   const [messages, setMessages] = useState([]);
+
+//   const handleUserClick = (userId: number) => {
+//     getMessages(userId).then((response) => {
+//       setMessages(response.data);
+//       console.log(response.data);
+//     });
+//   }
+
+//   useEffect(() => {
+//     getUsers().then((response) => {
+//       setUsers(response.data);
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       <ul>
+//         {/* {users.map((user) => (
+//           <li key={user.id}>
+//             <DefaultUserAvatar />
+//             {user.first_name} {user.last_name}
+//           </li>
+//         ))} */}
+
+//         {users.map((user) => (
+//           <li key={user.id} onClick={() => handleUserClick(user.id)}>
+//             <DefaultUserAvatar />
+//             {user.first_name} {user.last_name}
+//           </li>
+//         ))} 
+
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Page;
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const Page = () => {
-  const [users, setUsers] = useState<UserType[]>([]);
-  const [messages, setMessages] = useState([]);
-
-  const handleUserClick = (userId: number) => {
-    getMessages(userId).then((response) => {
-      setMessages(response.data);
-      console.log(response.data);
-    });
-  }
-
-  useEffect(() => {
-    getUsers().then((response) => {
-      setUsers(response.data);
-    });
-  }, []);
-
   return (
-    <div>
-      <h1>Users</h1>
-      <ul>
-        {/* {users.map((user) => (
-          <li key={user.id}>
-            <DefaultUserAvatar />
-            {user.first_name} {user.last_name}
-          </li>
-        ))} */}
+    <Tabs defaultValue="account">
+      
+      <div className="flex-col">
 
-        {users.map((user) => (
-          <li key={user.id} onClick={() => handleUserClick(user.id)}>
-            <DefaultUserAvatar />
-            {user.first_name} {user.last_name}
-          </li>
-        ))} 
+      <TabsList className="mt-10 ml-10">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
 
-      </ul>
-    </div>
-  );
+      <div className="w-full bg-red-600">
+      <TabsContent value="account">
+        <div className="w-full h-full p-4 bg-green-600">
+            asdfas
+        </div>
+
+      </TabsContent>
+      <TabsContent value="password">Change your password here.</TabsContent>
+      </div>
+      </div>
+      
+
+    </Tabs>
+
+  )
 }
 
 export default Page;
