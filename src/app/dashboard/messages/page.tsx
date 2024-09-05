5fdda3e1388d9844033339ca1af6535cc58b9485
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-05 23:36:47 +0500

    feat: messages

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -2,22 +2,59 @@
 
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import coreApi from "@/lib/coreApi";
 import Link from "next/link";
+
+const getUsers = async () => {
+  const response = await coreApi.get("/chats/users/");
+  return response.data;
+}
+
+const getDirectChats = async () => {
+  const response = await coreApi.get("/chats/direct/");
+  return response.data;
+}
+
+const getGroupChats = async () => {
+  const response = await coreApi.get("/chats/group/");
+  return response.data;
+}
 
 interface UserType {
   id: number;
   email: string;
 }
 
+interface DirectChatType {
+  id: number;
+  users: UserType[];
+}
+
+interface GroupChatType {
+  id: number;
+  users: UserType[];
+  title: string;
+  image: string;
+}
+
 const Page = () => {
   const [users, setUsers] = useState<UserType[]>([]);
+  const [directChats, setDirectChats] = useState<DirectChatType[]>([]);
+  const [groupChats, setGroupChats] = useState<GroupChatType[]>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
   
   useEffect(() => {
-    coreApi.get("/chats/users/").then((response) => {
-      setUsers(response.data);
+    Promise.all([
+      getUsers(),
+      getDirectChats(),
+      getGroupChats(),
+    ]).then(([users, directChats, groupChats]) => {
+      setUsers(users);
+      setDirectChats(directChats);
+      setGroupChats(groupChats);
     }).catch((error) => {
       setError(error.message);
     }).finally(() => {
@@ -28,17 +65,46 @@ const Page = () => {
   if (!isLoaded) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-  console.log(users);
-  
+  // console.log(users);
+  // console.log(directChats);
+  // console.log(groupChats);
+
   return (
-    <div className="flex flex-col space-y-4 p-4">
-      {users.map((user) => (
-        <Button key={user.id} asChild>
-          <Link href={`/dashboard/messages/${user.id}`}>
-            <span>{user.email}</span>
-          </Link>
-        </Button>
-      ))}
+    <div className="p-4 space-y-6">
+      <div className="flex flex-col space-y-4">
+        {users.map((user) => (
+          <Button key={user.id} asChild>
+            <Link href={`/dashboard/messages/${user.id}`}>
+              <span>{user.email}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+
+      <Separator className="dark:bg-white" />
+
+      <div className="flex flex-col space-y-4">
+        {directChats.map((chat) => (
+          <Button key={chat.id} asChild>
+            <Link href={`/dashboard/messages/direct-chats/${chat.id}`}>
+              <span>{chat.users.map((user) => user.email).join(", ")}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+
+      <Separator className="dark:bg-white" />
+
+      <div className="flex flex-col space-y-4">
+        {groupChats.map((chat) => (
+          <Button key={chat.id} asChild>
+            <Link href={`/dashboard/messages/group-chats/${chat.id}`}>
+              <span>{chat.title}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+
     </div>
   );
 }
