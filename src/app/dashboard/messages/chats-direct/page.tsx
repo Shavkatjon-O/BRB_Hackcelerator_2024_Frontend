4e2b0be059a1aef189566c4991d14689db656df5
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-07 02:15:55 +0500

    fix

--- src/app/dashboard/messages/chats-direct/page.tsx
+++ src/app/dashboard/messages/chats-direct/page.tsx
@@ -1,51 +1,9 @@
-"use client";
-
-import React from "react";
-import ChatListSidebar from "../_components/chat-list-sidebar";
-import { 
-  ResizablePanel,
-  ResizablePanelGroup,
-  ResizableHandle,
-} from "@/components/ui/resizable";
-import { useEffect, useState } from "react";
-import { getDirectChatList } from "../_services/chatsServices";
-import { DirectChatType } from "../_types/chatsTypes";
-import { cn } from "@/lib/utils";
-
-const Layout = ({ children }: { children: React.ReactNode} ) => {
-  const [chats, setChats] = useState<DirectChatType[]>([]);
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
-
-  useEffect(() => {
-    getDirectChatList().then((response) => {
-      setChats(response.data);
-    });
-  }, []);
-
+const Page = () => {
   return (
-    <div className="flex size-full">
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel 
-          collapsible={true}
-          onExpand={() => setIsCollapsed(false)}
-          onCollapse={() => setIsCollapsed(true)}
-          minSize={16}
-          maxSize={30}
-          className={cn(
-            isCollapsed && "min-w-28 transition-all duration-300 ease-in-out"
-          )}
-        >
-          <ChatListSidebar chats={chats} isCollapsed={isCollapsed} />
-        </ResizablePanel>
-
-        <ResizableHandle withHandle/>
-
-        <ResizablePanel>
-          {children}
-        </ResizablePanel>
-      </ResizablePanelGroup>
+    <div className="flex justify-center items-center size-full">
+      <h1>Select Chat to start Messaging</h1>
     </div>
   );
-}
+};
 
-export default Layout;
+export default Page;
