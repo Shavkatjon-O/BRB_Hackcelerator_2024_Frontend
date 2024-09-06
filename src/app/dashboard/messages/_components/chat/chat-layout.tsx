50c98736ea0d7f2b86c140b5b1f64f8e341b3316
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 23:52:21 +0500

    feat: chat list

--- src/app/dashboard/messages/_components/chat/chat-layout.tsx
+++ src/app/dashboard/messages/_components/chat/chat-layout.tsx
@@ -11,6 +11,9 @@ import { cn } from "@/lib/utils";
 import { Sidebar } from "../sidebar";
 import { Chat } from "./chat";
 
+import { DirectChatType, UserType, MessageType } from "../../_types/chatsTypes";
+import { getDirectChatList } from "../../_services/chatsServices";
+
 interface ChatLayoutProps {
   defaultLayout: number[] | undefined;
   defaultCollapsed?: boolean;
@@ -23,31 +26,43 @@ export function ChatLayout({
   navCollapsedSize,
 }: ChatLayoutProps) {
   const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed);
-  const [selectedUser, setSelectedUser] = React.useState(userData[0]);
+  const [selectedUser, setSelectedUser] = React.useState<DirectChatType | null>(null);
   const [isMobile, setIsMobile] = useState(false);
+  
+  const [directChats, setDirectChats] = useState<DirectChatType[]>([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
+  
   useEffect(() => {
+    getDirectChatList().then((response) => {
+      setDirectChats(response.data);
+      setSelectedUser(response.data[0]);
+      setIsLoaded(true);
+    });
+    
     const checkScreenWidth = () => {
       setIsMobile(window.innerWidth <= 768);
     };
-
+    
     // Initial check
     checkScreenWidth();
-
+    
     // Event listener for screen width changes
     window.addEventListener("resize", checkScreenWidth);
-
+    
     // Cleanup the event listener on component unmount
     return () => {
       window.removeEventListener("resize", checkScreenWidth);
     };
   }, []);
+  
+  if (!isLoaded) return null;
 
   return (
     <ResizablePanelGroup
-      direction="horizontal"
-      onLayout={(sizes: number[]) => {
-        document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+    direction="horizontal"
+    onLayout={(sizes: number[]) => {
+      document.cookie = `react-resizable-panels:layout=${JSON.stringify(
           sizes
         )}`;
       }}
@@ -77,22 +92,24 @@ export function ChatLayout({
       >
         <Sidebar
           isCollapsed={isCollapsed || isMobile}
-          chats={userData.map((user) => ({
-            name: user.name,
-            messages: user.messages ?? [],
-            avatar: user.avatar,
-            variant: selectedUser.name === user.name ? "secondary" : "ghost",
-          }))}
+          chats={
+            directChats.map((chat) => {
+              return {
+                ...chat,
+                variant: selectedUser && chat.id === selectedUser.id ? "secondary" : "ghost",
+              };
+            }) as DirectChatType[]
+          }
           isMobile={isMobile}
         />
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-        <Chat
+        {/* <Chat
           messages={selectedUser.messages}
           selectedUser={selectedUser}
           isMobile={isMobile}
-        />
+        /> */}
       </ResizablePanel>
     </ResizablePanelGroup>
   );
--- src/app/dashboard/messages/_components/sidebar.tsx
+++ src/app/dashboard/messages/_components/sidebar.tsx
@@ -11,21 +11,23 @@ import {
   TooltipProvider,
 } from "@/components/ui/tooltip";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Message } from "@/constants/data";
+
+import DefaultAvatar from "./default-avatar";
+import { DirectChatType } from "../_types/chatsTypes";
+import useUser from "@/hooks/useUser";
 
 interface SidebarProps {
   isCollapsed: boolean;
-  chats: {
-    name: string;
-    messages: Message[];
-    avatar: string;
-    variant: "secondary" | "ghost";
-  }[];
+  chats: any[];
   onClick?: () => void;
   isMobile: boolean;
 }
 
 export function Sidebar({ chats, isCollapsed, isMobile }: SidebarProps) {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded || !user) return null;
+
   return (
     <div
       data-collapsed={isCollapsed}
@@ -64,68 +66,147 @@ export function Sidebar({ chats, isCollapsed, isMobile }: SidebarProps) {
       <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
         {chats.map((chat, index) =>
           isCollapsed ? (
-            <TooltipProvider key={index}>
-              <Tooltip key={index} delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <Link
-                    href="#"
-                    className={cn(
-                      buttonVariants({ variant: chat.variant, size: "icon" }),
-                      "h-11 w-11 md:h-16 md:w-16",
-                      chat.variant === "secondary" &&
-                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-                    )}
+            chat.user1.id == user.id ? (
+              <TooltipProvider key={index}>
+                <Tooltip key={index} delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href="#"
+                      className={cn(
+                        buttonVariants({ variant: chat.variant, size: "icon" }),
+                        "h-11 w-11 md:h-16 md:w-16",
+                        chat.variant === "secondary" &&
+                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                      )}
+                    >
+                      {
+                        chat.user1.image ? (
+                          <Avatar className="flex justify-center items-center">
+                            <AvatarImage
+                              src={chat.user1.image}
+                              alt={chat.user1.image}
+                              width={6}
+                              height={6}
+                              className="w-10 h-10 "
+                            />
+                          </Avatar>
+                        ) : (
+                          <DefaultAvatar />
+                        )
+                      } { " " }
+                      <span className="sr-only">{chat.user1.email}</span>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="right"
+                    className="flex items-center gap-4"
                   >
+                    {chat.user1.email}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            ) : (
+              <TooltipProvider key={index}>
+                <Tooltip key={index} delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href="#"
+                      className={cn(
+                        buttonVariants({ variant: chat.variant, size: "icon" }),
+                        "h-11 w-11 md:h-16 md:w-16",
+                        chat.variant === "secondary" &&
+                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                      )}
+                    >
+                      {
+                        chat.user2.image ? (
+                          <Avatar className="flex justify-center items-center">
+                            <AvatarImage
+                              src={chat.user2.image}
+                              alt={chat.user2.image}
+                              width={6}
+                              height={6}
+                              className="w-10 h-10 "
+                            />
+                          </Avatar>
+                        ) : (
+                          <DefaultAvatar />
+                        )
+                      } { " " }
+                      <span className="sr-only">{chat.user2.email}</span>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="right"
+                    className="flex items-center gap-4"
+                  >
+                    {chat.user2.email}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )
+          ) : (
+            chat.user1.id == user.id ? (
+              <Link
+                key={index}
+                href="#"
+                className={cn(
+                  buttonVariants({ variant: chat.variant, size: "lg" }),
+                  chat.variant === "secondary" &&
+                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white shrink",
+                  "justify-start gap-4"
+                )}
+              >
+                {
+                  chat.user1.image ? (
                     <Avatar className="flex justify-center items-center">
                       <AvatarImage
-                        src={chat.avatar}
-                        alt={chat.avatar}
+                        src={chat.user1.image}
+                        alt={chat.user1.image}
                         width={6}
                         height={6}
                         className="w-10 h-10 "
                       />
-                    </Avatar>{" "}
-                    <span className="sr-only">{chat.name}</span>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="flex items-center gap-4"
-                >
-                  {chat.name}
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          ) : (
-            <Link
-              key={index}
-              href="#"
-              className={cn(
-                buttonVariants({ variant: chat.variant, size: "lg" }),
-                chat.variant === "secondary" &&
-                "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white shrink",
-                "justify-start gap-4"
-              )}
-            >
-              <Avatar className="flex justify-center items-center">
-                <AvatarImage
-                  src={chat.avatar}
-                  alt={chat.avatar}
-                  width={6}
-                  height={6}
-                  className="w-10 h-10 "
-                />
-              </Avatar>
-              <div className="flex flex-col max-w-28">
-                <span>{chat.name}</span>
-                {chat.messages.length > 0 && (
-                  <span className="text-zinc-300 text-xs truncate ">
-                    {chat.messages[chat.messages.length - 1].name.split(" ")[0]}
-                    : {chat.messages[chat.messages.length - 1].isLoading ? "Typing..." : chat.messages[chat.messages.length - 1].message}
-                  </span>
+                    </Avatar>
+                  ) : (
+                    <DefaultAvatar />
+                  )
+                }
+                <div className="flex flex-col max-w-28">
+                  <span>{chat.user1.email}</span>
+                </div>
+              </Link>
+            ) : (
+              <Link
+                key={index}
+                href="#"
+                className={cn(
+                  buttonVariants({ variant: chat.variant, size: "lg" }),
+                  chat.variant === "secondary" &&
+                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white shrink",
+                  "justify-start gap-4"
                 )}
-              </div>
-            </Link>
+              >
+                {
+                  chat.user2.image ? (
+                    <Avatar className="flex justify-center items-center">
+                      <AvatarImage
+                        src={chat.user2.image}
+                        alt={chat.user2.image}
+                        width={6}
+                        height={6}
+                        className="w-10 h-10 "
+                      />
+                    </Avatar>
+                  ) : (
+                    <DefaultAvatar />
+                  )
+                }
+                <div className="flex flex-col max-w-28">
+                  <span>{chat.user2.email}</span>
+                </div>
+              </Link>
+            )
           )
         )}
       </nav>
