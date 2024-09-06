47c9d6b9df69d8fb73e9583741e81ed007c86fc8
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 17:14:06 +0500

    feat: new structure

---
+++ src/app/dashboard/messages/_components/chat-type-list-sidebar.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { User, Users, Search, House } from "lucide-react";
+import Link from "next/link";
+
+const ChatTypeSidebar = () => {
+  return (
+    <div className="h-full flex flex-col p-4 space-y-4 border-r">
+      <Button size="icon" variant="outline" asChild>
+        <Link href="/dashboard/messages">
+          <House className="w-[1.2rem] h-[1.2rem]" />
+        </Link>
+      </Button>
+      <Button size="icon" variant="outline" asChild>
+        <Link href="/dashboard/messages/chats-search">
+          <Search className="w-[1.2rem] h-[1.2rem]" />
+        </Link>
+      </Button>
+      <Button size="icon" variant="outline" asChild>
+        <Link href="/dashboard/messages/chats-direct">
+          <User className="w-[1.2rem] h-[1.2rem]" />
+        </Link>
+      </Button>
+      <Button size="icon" variant="outline" asChild>
+        <Link href="/dashboard/messages/chats-group">
+          <Users className="w-[1.2rem] h-[1.2rem]" />
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default ChatTypeSidebar;
