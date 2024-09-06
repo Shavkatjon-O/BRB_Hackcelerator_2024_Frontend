95f603b85d74bdc89a58f4d8853a2b78dc33c9bf
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 18:22:31 +0500

    fix: chats

--- src/app/dashboard/messages/_components/chat-type-list-sidebar.tsx
+++ src/app/dashboard/messages/_components/chat-type-list-sidebar.tsx
@@ -37,7 +37,7 @@ const ChatTypeListSidebar: React.FC = () => {
           tooltipText="All Chats" 
         />
         <SidebarItem 
-          href="/dashboard/messages/chats-search" 
+          href="/dashboard/messages/chats-explore" 
           icon={Search} 
           tooltipText="Search People" 
         />
--- src/app/dashboard/messages/chats-direct/layout.tsx
+++ src/app/dashboard/messages/chats-direct/layout.tsx
@@ -1,26 +1,26 @@
-// "use client";
+"use client";
 
-// import React from "react";
-// import ChatListSidebar from "../_components/chat-list-sidebar";
-// import { useEffect, useState } from "react";
-// import { getDirectChatList } from "../_services/messagesServices";
-// import { DirectChatType } from "../_types/messagesTypes";
+import React from "react";
+import ChatListSidebar from "../_components/chat-list-sidebar";
+import { useEffect, useState } from "react";
+import { getDirectChatList } from "../_services/chatsServices";
+import { DirectChatType } from "../_types/chatsTypes";
 
-// const Layout = ({ children }: { children: React.ReactNode} ) => {
-//   const [chats, setChats] = useState<DirectChatType[]>([]);
+const Layout = ({ children }: { children: React.ReactNode} ) => {
+  const [chats, setChats] = useState<DirectChatType[]>([]);
 
-//   useEffect(() => {
-//     getDirectChatList().then((response) => {
-//       setChats(response.data);
-//     });
-//   }, []);
+  useEffect(() => {
+    getDirectChatList().then((response) => {
+      setChats(response.data);
+    });
+  }, []);
 
-//   return (
-//     <div className="flex size-full">
-//       <ChatListSidebar chats={chats} />
-//       {children}
-//     </div>
-//   );
-// }
+  return (
+    <div className="flex size-full">
+      <ChatListSidebar chats={chats} />
+      {children}
+    </div>
+  );
+}
 
-// export default Layout;
+export default Layout;
