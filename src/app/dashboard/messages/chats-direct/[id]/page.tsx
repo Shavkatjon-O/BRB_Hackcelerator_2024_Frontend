0ca7186457f9dfc667ea6182419483952e08dafd
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 12:57:18 +0500

    feat: chat page

--- src/app/dashboard/messages/chats-direct/[id]/page.tsx
+++ src/app/dashboard/messages/chats-direct/[id]/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { 
+  getDirectChat, 
+  getDirectChatMessageList, 
+  createDirectChatMessage 
+} from "../../_services/messagesServices";
+import useUser from "@/hooks/useUser";
+
+
 const Page = () => {
+  const { id } = useParams();
+  const { user, isLoaded, error } = useUser();
+  const [chat, setChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
   return (
     <div>
       <h1>Chat</h1>
