9e9ca65ddd6ad88288eb888559bbfd57d4696670
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 19:02:42 +0500

    feat: scroll area in chat list

--- src/app/dashboard/messages/_components/chat-list-sidebar.tsx
+++ src/app/dashboard/messages/_components/chat-list-sidebar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { DirectChatType } from "../_types/chatsTypes";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import DefaultAvatar from "./default-avatar";
-import { Separator } from "@/components/ui/separator";
 import useUser from "@/hooks/useUser";
 import Image from "next/image";
 
@@ -30,22 +30,25 @@ const ChatListSidebar = ({ chats }: ChatListSidebarProps) => {
   );
 
   return (
-    <div className="min-w-80 py-4 h-full border-r overflow-y-scroll">
-      <div className="border-t">
+    <div className="min-w-80 py- h-full border-r">
+      <ScrollArea className="h-full">
         {chats.map((chat) => {
           const chatUser = chat.user1.id === Number(user?.id) ? chat.user2 : chat.user1;
 
           return (
-            <>
-              <Button key={chat.id} variant="ghost" asChild className="w-full py-10 flex justify-start rounded-none border-b">
-                <Link href={`/dashboard/messages/chats-direct/${chat.id}`}>
-                  {isLoaded && user ? renderChatUser(chatUser) : <div>Loading...</div>}
-                </Link>
-              </Button>
-            </>
+            <Button
+              key={chat.id}
+              variant="ghost"
+              asChild
+              className="w-full py-10 flex justify-start rounded-none border-b"
+            >
+              <Link href={`/dashboard/messages/chats-direct/${chat.id}`}>
+                {isLoaded && user ? renderChatUser(chatUser) : <div>Loading...</div>}
+              </Link>
+            </Button>
           );
         })}
-      </div>
+      </ScrollArea>
     </div>
   );
 };
