60bb6e44900c8a43dcb7e04dc690993852d57c83
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 16:31:27 +0500

    fix: chats

--- src/app/dashboard/messages/_components/chat-sidebar.tsx
+++ src/app/dashboard/messages/_components/chat-sidebar.tsx
@@ -1,31 +1,94 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { User, Users, Search, House } from "lucide-react";
+import { useState, useEffect } from "react";
+import { DirectChatType } from "../_types/messagesTypes";
+import { Button } from "@/components/ui/button";
+import DefaultAvatar from "./default-avatar";
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+import { getDirectChatList } from "../_services/messagesServices"; 
+import Link from "next/link";
 
 const ChatSidebar = () => {
+  const { user, isLoaded } = useUser();
+  const [directChats, setDirectChats] = useState<DirectChatType[]>([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      if (isLoaded && user) {
+        try {
+          const { data } = await getDirectChatList(); 
+          setDirectChats(data);
+        } catch (error) {
+          console.error("Failed to fetch direct chats", error);
+        }
+      }
+    };
+
+    fetchChats();
+  }, [isLoaded, user]);
+
+  const renderChatUser = (chatUser: { id: number; image?: string; email: string }) => (
+    <div className="flex items-center gap-2 overflow-x-hidden">
+      <div>
+        {chatUser.image ? (
+          <Image src={chatUser.image} alt={chatUser.email} width={40} height={40} />
+        ) : (
+          <DefaultAvatar />
+        )}
+      </div>
+      <div className="flex flex-col">
+        <span>{chatUser.email}</span>
+        <span className="text-sm text-green-500">Online</span>
+      </div>
+    </div>
+  );
+
   return (
     <div className="h-full w-full p-4">
-      <Tabs defaultValue="chats-all" className="w-full">
+      <Tabs defaultValue="chats-all" className="w-full space-y-4">
         <TabsList className="w-full min-h-14 h-full flex flex-wrap">
           <TabsTrigger value="chats-all" className="flex-1 min-w-[4rem] gap-1 p-4">
             <House className="w-[1.2rem] h-[1.2rem]" />
           </TabsTrigger>
-
           <TabsTrigger value="chats-direct" className="flex-1 min-w-[4rem] gap-1 p-4">
             <User className="w-[1.2rem] h-[1.2rem]" />
           </TabsTrigger>
-
           <TabsTrigger value="chats-group" className="flex-1 min-w-[4rem] gap-1 p-4">
             <Users className="w-[1.2rem] h-[1.2rem]" />
           </TabsTrigger>
-
           <TabsTrigger value="search-people" className="flex-1 min-w-[4rem] gap-1 p-4">
             <Search className="w-[1.2rem] h-[1.2rem]" />
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="chats-all"></TabsContent>
-        <TabsContent value="chats-direct"></TabsContent>
-        <TabsContent value="chats-group"></TabsContent>
-        <TabsContent value="search-people"></TabsContent>
+        <TabsContent value="chats-all">
+          {/* Add content for all chats if needed */}
+        </TabsContent>
+        <TabsContent value="chats-direct">
+          <div className="w-full h-full overflow-y-scroll">
+            <div className="space-y-4">
+              {directChats.map((chat) => {
+                const chatUser = chat.user1.id === Number(user?.id) ? chat.user2 : chat.user1;
+
+                return (
+                  <Button key={chat.id} variant="outline" asChild className="w-full py-8 flex justify-start">
+                    <Link href={`/dashboard/messages/chats-direct/${chat.id}`}>
+                      {isLoaded && user ? renderChatUser(chatUser) : <div>Loading...</div>}
+                    </Link>
+                  </Button>
+                );
+              })}
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="chats-group">
+          {/* Add content for group chats if needed */}
+        </TabsContent>
+        <TabsContent value="search-people">
+          {/* Add content for searching people if needed */}
+        </TabsContent>
       </Tabs>
     </div>
   );
