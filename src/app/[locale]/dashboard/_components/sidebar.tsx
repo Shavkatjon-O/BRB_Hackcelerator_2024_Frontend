9861debab12e5706cfeea5135d55b0d9619270e8
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-10 01:08:21 +0500

    fix: ui

--- src/app/[locale]/dashboard/_components/sidebar.tsx
+++ src/app/[locale]/dashboard/_components/sidebar.tsx
@@ -34,6 +34,7 @@ import {
 import { usePathname } from "next/navigation";
 import useUser from "@/hooks/useUser";
 import { cn } from "@/lib/utils";
+import { Separator } from '@/components/ui/separator';
 
 
 const bankTellerLinks = [
@@ -42,6 +43,29 @@ const bankTellerLinks = [
   { href: "/dashboard/payments", label: "Payments", icon: BadgeDollarSign },
   { href: "/dashboard/upload", label: "Upload", icon: Upload },
 ];
+
+
+const defaultLinks = {
+  "General": [
+    { href: "/dashboard", label: "Home", icon: House },
+    { href: "/dashboard/notifications", label: "Notifications", icon: Bell },
+    { href: "/dashboard/messages", label: "Messages", icon: MessageSquareText },
+    { href: "/dashboard/requests", label: "Requests & Approvals", icon: FileCheck },
+    { href: "/dashboard/announcements", label: "Announcements", icon: FileCheck },
+    { href: "/dashboard/events", label: "Events & Calendar", icon: Calendar },
+    { href: "/dashboard/documents", label: "Documents", icon: FileCheck },
+    { href: "/dashboard/meetings", label: "Video & Call Meetings", icon: CalendarCheck },
+    { href: "/dashboard/tasks", label: "Tasks Management", icon: SquareCheckBig },
+    { href: "/dashbaord/reports", label: "Reports", icon: FileCheck },
+    { href: "/dashboard/assistants", label: "AI Assistant", icon: Bot },
+  ],
+  "Other": [
+    { href: "/dashboard/profile", label: "Profile", icon: User },
+    { href: "/dashboard/settings", label: "Settings", icon: Settings },
+    { href: "/dashboard/help", label: "Help", icon: HelpCircle },
+    { href: "/sign-out", label: "Sign Out", icon: LogOut },
+  ],
+}
 
 const baseLinks = [
   // General
@@ -105,7 +129,7 @@ const Sidebar = () => {
 
         <ScrollArea>
           <nav className="flex flex-col max-h-full space-y-2 p-4">
-            {links.map(({ href, label, icon: Icon }) => (
+            {/* {links.map(({ href, label, icon: Icon }) => (
               <Button
                 key={href}
                 asChild
@@ -124,7 +148,40 @@ const Sidebar = () => {
                   </span>
                 </Link>
               </Button>
-            ))}
+            ))} */}
+
+            {Object.entries(defaultLinks).map(([title, links]) => (
+              <div key={title} className="space-y-2">
+                {
+                  isOpen ? (
+                    <h3 className="text-sm text-slate-500 dark:text-slate-300">{title}</h3>
+                  ) : (
+                    <Separator />
+                  )
+                }
+                {links.map(({ href, label, icon: Icon }) => (
+                  <Button
+                    key={href}
+                    asChild
+                    variant={pathname === href ? "default" : "ghost"}
+                    className={`w-full py-2 px-3 h-full flex ${
+                      cn(
+                        isOpen ? "justify-start" : "justify-center",
+                        pathname !== href ? "text-slate-500 dark:text-slate-300" : ""
+                      )
+                    }`}
+                  >
+                    <Link href={href} className="flex items-center space-x-2 w-full">
+                      <Icon size={24} className="h-5 w-5" />
+                      <span className={`${isOpen ? "block" : "hidden"} ml-2`}>
+                        {label}
+                      </span>
+                    </Link>
+                  </Button>
+                ))}
+              </div>
+            ), [])} 
+
           </nav>
         </ScrollArea>
 
