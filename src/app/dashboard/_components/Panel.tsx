188f9e65a0ff378e413d2f14cbd8e34504e989eb
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-12 14:36:53 +0500

    fix: responsive design

--- src/app/dashboard/_components/Panel.tsx
+++ src/app/dashboard/_components/Panel.tsx
@@ -8,8 +8,8 @@ interface Props extends React.ComponentPropsWithoutRef<"div"> {
 const Panel = ({ children, title, className, action, ...props }: Props) => {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center justify-between">
-        <h1 className="text-lg font-semibold md:text-2xl">{title}</h1>
+      <div className="flex items-center justify-between flex-col lg:flex-row">
+        <h1 className="mb-4 lg:mb-0 text-lg font-semibold md:text-2xl">{title}</h1>
         {action}
       </div>
       <div className={cn("", className)} {...props}>
--- src/app/dashboard/announcements/page.tsx
+++ src/app/dashboard/announcements/page.tsx
@@ -18,7 +18,7 @@ const TabsComponent = ({ onFilterChange }: { onFilterChange: (filter: string) =>
 
   return (
     <Tabs defaultValue="all" onValueChange={handleTabChange}>
-      <TabsList className="flex gap-2 bg-slate-200 dark:bg-slate-700">
+      <TabsList className="flex gap-0 lg:gap-2 bg-slate-200 dark:bg-slate-700">
         <TabsTrigger value="all">All Time</TabsTrigger>
         <TabsTrigger value="last7days">Last 7 Days</TabsTrigger>
         <TabsTrigger value="last30days">Last 30 Days</TabsTrigger>
