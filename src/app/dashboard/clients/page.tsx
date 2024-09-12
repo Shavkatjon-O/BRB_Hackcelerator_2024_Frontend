6f0679f57f3e9a473f6bd23ac40bf9818dd6723c
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-12 09:27:06 +0500

    fix: margin

--- src/app/dashboard/clients/page.tsx
+++ src/app/dashboard/clients/page.tsx
@@ -196,7 +196,7 @@ const Page = () => {
   return (
     <Panel title="Clients">
       <div className="w-full">
-        <div className="flex items-center py-4">
+        <div className="flex items-center mb-4">
           <Input
             placeholder="Filter emails..."
             value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
