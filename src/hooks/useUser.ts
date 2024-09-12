7bc1b5ff6331aed990a08efcdc0900b76b5e1de1
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-12 09:24:47 +0500

    fix: infinite loading state

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,49 +1,27 @@
-// "use client";
-
-// import CoreAPI from "@/lib/coreApi";
-// import { useEffect, useState } from "react";
-// import { UserProfileType } from "@/types/authTypes";
-// import Cookies from "js-cookie";
-
-// const useUser = () => {
-//   const [user, setUser] = useState<UserProfileType | null>(null);
-//   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-//   const [error, setError] = useState<any>(null);
-
-//   useEffect(() => {
-//     const fetchUserProfile = async () => {
-//       try {
-//         const response = await CoreAPI.get("/users/profile/");
-//         setUser(response.data);
-//       } catch (error: any) {
-//         setError(error.message);
-//       } finally {
-//         setIsLoaded(true);
-//       }
-//     }
-//     fetchUserProfile();
-//   }, []);
-
-//   return { user, isLoaded, error };
-// }
-
-// export default useUser;
-
-// hooks/useUser.ts
-
 "use client";
 
-import { useEffect } from "react";
-import useUserStore from "@/stores/userStore";
+import CoreAPI from "@/lib/coreApi";
+import { useEffect, useState } from "react";
+import { UserProfileType } from "@/types/authTypes";
 
 const useUser = () => {
-  const { user, isLoaded, error, fetchUser } = useUserStore();
+  const [user, setUser] = useState<UserProfileType | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [error, setError] = useState<any>(null);
 
   useEffect(() => {
-    if (!isLoaded) {
-      fetchUser();
+    const fetchUserProfile = async () => {
+      try {
+        const response = await CoreAPI.get("/users/profile/");
+        setUser(response.data);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setIsLoaded(true);
+      }
     }
-  }, [isLoaded, fetchUser]);
+    fetchUserProfile();
+  }, []);
 
   return { user, isLoaded, error };
 }
