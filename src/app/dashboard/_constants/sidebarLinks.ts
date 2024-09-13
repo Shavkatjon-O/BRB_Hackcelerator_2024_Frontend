ac74038fc9fdd24f0b3612a71c8854a73342c70c
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-13 08:33:15 +0500

    fix

--- src/app/dashboard/_constants/sidebarLinks.ts
+++ src/app/dashboard/_constants/sidebarLinks.ts
@@ -16,20 +16,20 @@ import {
 
 export const defaultLinks = {
   "General": [
-    { href: "/dashboard/notifications", label: "Notifications", icon: Bell },
+    // { href: "/dashboard/notifications", label: "Notifications", icon: Bell },
     { href: "/dashboard/messages", label: "Messages", icon: MessageSquareText },
     { href: "/dashboard/requests", label: "Requests & Approvals", icon: CheckSquare },
     { href: "/dashboard/announcements", label: "Announcements", icon: Megaphone },
     { href: "/dashboard/events", label: "Events & Calendar", icon: Calendar },
-    { href: "/dashboard/documents", label: "Documents", icon: File },
+    // { href: "/dashboard/documents", label: "Documents", icon: File },
     { href: "/dashboard/meetings", label: "Video & Call Meetings", icon: Camera },
     { href: "/dashboard/tasks", label: "Tasks Management", icon: ListChecks },
     { href: "/dashboard/reports", label: "Reports", icon: PieChart },
   ],
   "Other": [
     { href: "/dashboard/profile", label: "Profile", icon: User },
-    { href: "/dashboard/settings", label: "Settings", icon: Settings },
-    { href: "/dashboard/help", label: "Help", icon: HelpCircle },
+    // { href: "/dashboard/settings", label: "Settings", icon: Settings },
+    // { href: "/dashboard/help", label: "Help", icon: HelpCircle },
     { href: "/sign-out", label: "Sign Out", icon: LogOut },
   ],
 }
