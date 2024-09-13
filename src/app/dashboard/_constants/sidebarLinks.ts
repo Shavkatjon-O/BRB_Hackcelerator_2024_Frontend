cd9356dfcccf1ada97ea683fb3d159335824f2ea
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-13 09:01:49 +0500

    fix

--- src/app/dashboard/_constants/sidebarLinks.ts
+++ src/app/dashboard/_constants/sidebarLinks.ts
@@ -18,12 +18,12 @@ export const defaultLinks = {
   "General": [
     // { href: "/dashboard/notifications", label: "Notifications", icon: Bell },
     { href: "/dashboard/messages", label: "Messages", icon: MessageSquareText },
-    { href: "/dashboard/requests", label: "Requests & Approvals", icon: CheckSquare },
-    { href: "/dashboard/announcements", label: "Announcements", icon: Megaphone },
+    { href: "/dashboard/meetings", label: "Video & Call Meetings", icon: Camera },
     { href: "/dashboard/events", label: "Events & Calendar", icon: Calendar },
     // { href: "/dashboard/documents", label: "Documents", icon: File },
-    { href: "/dashboard/meetings", label: "Video & Call Meetings", icon: Camera },
-    { href: "/dashboard/tasks", label: "Tasks Management", icon: ListChecks },
+    { href: "/dashboard/requests", label: "Requests & Approvals", icon: CheckSquare },
+    { href: "/dashboard/announcements", label: "Announcements", icon: Megaphone },
+    { href: "/dashboard/tasks", label: "Tasks", icon: ListChecks },
     // { href: "/dashboard/reports", label: "Reports", icon: PieChart },
   ],
   "Other": [
