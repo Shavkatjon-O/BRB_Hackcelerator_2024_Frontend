f19dd23172dc3575332da8e06fee26ec9fdf8a76
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-15 00:52:52 +0500

    fix

--- src/app/[locale]/dashboard/announcements/[id]/page.tsx
+++ src/app/[locale]/dashboard/announcements/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Panel from "../_components/Panel";
+import Panel from "../../_components/Panel";
 import { useParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
